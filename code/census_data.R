e64d8a41b2716a6d70e63d9cc440efeddb5b1247
Forest Gregg <fgregg@uchicago.edu>
2013-07-16 15:39:21 -0500

    write out trainging data to file

--- code/census_data.R
+++ code/census_data.R
@@ -1,24 +1,4 @@
 library(RJSONIO)
-library(rgdal)
-library(rgeos)
-library(spdep)
-library(igraph)
-
-base_url <- "http://api.census.gov/data/2010/sf1?key=ac94ba69718a7e1da4f89c6d218b8f6b5ae9ac49"
-
-
-nb2edgelist <- function(nb) {
-  el <- c()
-  for (i in 1:length(nb)) {
-    neighbors <- nb[[i]]
-    new.neighbors <- neighbors[neighbors > i]
-    if (length(new.neighbors) > 0) {
-      el <- rbind(el, cbind(i, new.neighbors))
-    }
-  }
-  return(el)
-}
-
 
 censusData <- function(json_file, variables=NULL) {
   json_data <- fromJSON(json_file)
@@ -83,42 +63,7 @@ chicago_blocks <- blockData('17', '031', variables)
 chicago_blocks$TRACT_BLOC = paste(chicago_blocks$tract,
                                   chicago_blocks$block,
                                   sep="")
-               
-# Load Block Data
-blocks.poly <- readOGR("../admin_areas/CensusBlockTIGER2010.shp",
-                       "CensusBlockTIGER2010")
 
-# Merge Census data with Geo Spatial Data Frame
-blocks.poly@data = data.frame(blocks.poly@data,
-                              chicago_blocks[match(blocks.poly@data[,c("TRACT_BLOC")],
-                                                   chicago_blocks[, c("TRACT_BLOC")]),])
-
-
-# Topology of Block Connectivity
-neighbors <-poly2nb(blocks.poly,
-                    foundInBox=gUnarySTRtreeQuery(blocks.poly))
-
-edgelist <- nb2edgelist(neighbors)
-
-
-for (i in 1:length(neighbors)) {
-  p <- blocks.poly@data[i, c("P0040003", "P0050003", "P0050004",
-                             "P0050005", "P0050006", "P0050007",
-                             "P0050008", "P0050009")] + 1
-  p <- p/(blocks.poly@data[i, "P0040001"] + 8)
-  for (j in neighbors[[i]]) {
-    q <- blocks.poly@data[j, c("P0040003", "P0050003", "P0050004",
-                             "P0050005", "P0050006", "P0050007",
-                             "P0050008", "P0050009")] + 1
-    q <- q/(blocks.poly@data[j, "P0040001"] + 8)
-
-    # Jensen-Shannon divergence
-    # http://en.wikipedia.org/wiki/Jensen%E2%80%93Shannon_divergence
-    m <- 0.5 * (p + q)
-    js <- (0.5 * sum(unlist(ifelse(p==0, 0, log(p/m)*p)))
-           + 0.5 * sum(unlist(ifelse(q==0, 0, log(q/m)*q))))
-    print(js)
-  }
-  break
-}
-
+write.csv(chicago_blocks,
+          "census_data_blocks.csv",
+          row.names=FALSE)
