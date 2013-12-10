6afe874ea75b85be143cad7d27bc557d67a564b5
Forest Gregg <fgregg@uchicago.edu>
2013-12-09 18:08:10 -0600

    start qualifying_paper.Rnw

---
+++ code/requirements.R
@@ -0,0 +1,7 @@
+install.packages("knitr")
+install.packages("rgeos")
+install.packages("spdep")
+install.packages("devtools")
+install.packages("ks")
+install.packages("rgdal")
+install.packages("igraph")
