8ce215363218f825ff26ba8818404bc00cdc394c
Forest Gregg <fgregg@uchicago.edu>
2013-12-18 16:20:48 -0600

    predictions for chicago

--- code/training/features.R
+++ code/training/features.R
@@ -9,22 +9,12 @@ js_race <- read.csv('../interchange/js_race.csv')$x
 js_housing <- read.csv('../interchange/js_housing.csv')$x
 
 rail <- read.csv('../interchange/rail_intersects.csv')$x
-#rail <- read.csv('../interchange/rail_distance.csv')$x^(1/2)
 highway <- read.csv('../interchange/highway_intersects.csv')$x
-#highway <- read.csv('../interchange/highway_distance.csv')$x^(1/2)
 grid_street <- read.csv('../interchange/grid_intersects.csv')$x
-#grid_street <- read.csv('../interchange/grid_distance.csv')$x^(1/2)
 water <- read.csv('../interchange/water_intersects.csv')$x
-#water <- read.csv('../interchange/water_distance.csv')$x^(1/2)
 
-zoning <- read.csv('../interchange/zoning_crosses.csv')$x
 elementary_school <- read.csv('../interchange/elementary_schools_crosses.csv')$x
-#elementary_school <- read.csv('../interchange/elementary_schools_distances.csv')$x
-#elementary_school <- elementary_school^(1/2)
 high_school <- read.csv('../interchange/high_schools_crosses.csv')$x
-#high_school <- read.csv('../interchange/high_schools_distances.csv')$x
-#high_school <- high_school^(1/2)
-
 block_angle <- read.csv('../interchange/block_angles.csv')$x
 
 block_angle <- block_angle %% pi/2 / (pi/2)
@@ -57,7 +47,6 @@ M <- model.matrix(node_labels ~ sufficient_pop:(
                                 sufficient_pop*( 
                                   rail +
                                   water +
-                                  #zoning +
                                   elementary_school +
                                   high_school +
                                   block_angle),
