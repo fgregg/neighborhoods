fcee7794a1d8b88afdc62f1a0aaa1fef26ff2341
Forest Gregg <fgregg@uchicago.edu>
2013-12-31 10:59:14 -0600

    sync

--- code/training/train_ssvm.py
+++ code/training/train_ssvm.py
@@ -21,8 +21,6 @@ edge_attributes = numpy.loadtxt('model.matrix', skiprows=1)
 edges = numpy.loadtxt(PATH + 'edges.csv', skiprows = 1, dtype=int, delimiter=',')
 edges += -1
 
-markers = numpy.transpose(numpy.vstack((numpy.arange(0,len(node_labels)), node_labels)))
-
 Y = (numpy.array(node_labels, dtype=numpy.int),)
 X = numpy.empty((len(node_labels), 0), dtype=numpy.float)
 
@@ -32,14 +30,14 @@ X = ((X, E, edge_attributes),)
 model = PottsEdgeFeatureGraphCRF(n_states = 23, 
                                  n_features = 0,
                                  n_edge_features = edge_attributes.shape[1],
-                                 inference_method = 'qpbo',
-                                 markers=markers)
+                                 inference_method = 'ad3')
 
 
 svm = learner(model, 
               verbose=3, 
-              n_jobs=5, 
+              n_jobs=1, 
               max_iter=1000, 
+              switch_to='qpbo',
               C=args.regularizer[0], 
               show_loss_every=1)
 
@@ -57,15 +55,15 @@ with open('/home/fgregg/academic/neighborhoods/code/training/predicted_borders.c
    for edge in predicted_borders[0] :
       writer.writerow([edge])
 
-chicago_nodes_n = len(numpy.loadtxt(PATH + 'chicago_node_labels.csv', skiprows = 1))
-chicago_edges = numpy.loadtxt(PATH + 'chicago_edges.csv', skiprows = 1, dtype=int, delimiter=',')
-X_chicago = ((numpy.empty((chicago_nodes_n, 0), dtype=numpy.float), 
-              numpy.array(chicago_edges, dtype=numpy.int),
-              numpy.loadtxt('chicago.model.matrix', skiprows=1)),)
+# chicago_nodes_n = len(numpy.loadtxt(PATH + 'chicago_node_labels.csv', skiprows = 1))
+# chicago_edges = numpy.loadtxt(PATH + 'chicago_edges.csv', skiprows = 1, dtype=int, delimiter=',')
+# X_chicago = ((numpy.empty((chicago_nodes_n, 0), dtype=numpy.float), 
+#               numpy.array(chicago_edges, dtype=numpy.int),
+#               numpy.loadtxt('chicago.model.matrix', skiprows=1)),)
 
-predicted_chicago = svm.predict(X_chicago)
+# predicted_chicago = svm.predict(X_chicago)
                          
-with open('/home/fgregg/academic/neighborhoods/code/training/predicted_chicago.csv', 'w') as f :
-   writer = csv.writer(f, delimiter=' ')
-   for edge in predicted_chicago[0] :
-      writer.writerow([edge])
+# with open('/home/fgregg/academic/neighborhoods/code/training/predicted_chicago.csv', 'w') as f :
+#    writer = csv.writer(f, delimiter=' ')
+#    for edge in predicted_chicago[0] :
+#       writer.writerow([edge])
