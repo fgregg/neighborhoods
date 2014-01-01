71451506f2d37480985b28194146a22632f075da
Forest Gregg <fgregg@uchicago.edu>
2014-01-01 16:29:50 -0600

    fixed bug that was creating segfault

--- code/training/train_ssvm.py
+++ code/training/train_ssvm.py
@@ -30,14 +30,13 @@ X = ((X, E, edge_attributes),)
 model = PottsEdgeFeatureGraphCRF(n_states = 23, 
                                  n_features = 0,
                                  n_edge_features = edge_attributes.shape[1],
-                                 inference_method = 'ad3')
+                                 inference_method = 'qpbo')
 
 
 svm = learner(model, 
               verbose=3, 
               n_jobs=1, 
               max_iter=1000, 
-              switch_to='qpbo',
               C=args.regularizer[0], 
               show_loss_every=1)
 
@@ -55,15 +54,15 @@ with open('/home/fgregg/academic/neighborhoods/code/training/predicted_borders.c
    for edge in predicted_borders[0] :
       writer.writerow([edge])
 
-# chicago_nodes_n = len(numpy.loadtxt(PATH + 'chicago_node_labels.csv', skiprows = 1))
-# chicago_edges = numpy.loadtxt(PATH + 'chicago_edges.csv', skiprows = 1, dtype=int, delimiter=',')
-# X_chicago = ((numpy.empty((chicago_nodes_n, 0), dtype=numpy.float), 
-#               numpy.array(chicago_edges, dtype=numpy.int),
-#               numpy.loadtxt('chicago.model.matrix', skiprows=1)),)
+chicago_nodes_n = len(numpy.loadtxt(PATH + 'chicago_node_labels.csv', skiprows = 1))
+chicago_edges = numpy.loadtxt(PATH + 'chicago_edges.csv', skiprows = 1, dtype=int, delimiter=',')
+X_chicago = ((numpy.empty((chicago_nodes_n, 0), dtype=numpy.float), 
+              numpy.array(chicago_edges, dtype=numpy.int) - 1,
+              numpy.loadtxt('chicago.model.matrix', skiprows=1)),)
 
-# predicted_chicago = svm.predict(X_chicago)
+predicted_chicago = svm.predict(X_chicago)
                          
-# with open('/home/fgregg/academic/neighborhoods/code/training/predicted_chicago.csv', 'w') as f :
-#    writer = csv.writer(f, delimiter=' ')
-#    for edge in predicted_chicago[0] :
-#       writer.writerow([edge])
+with open('/home/fgregg/academic/neighborhoods/code/training/predicted_chicago.csv', 'w') as f :
+   writer = csv.writer(f, delimiter=' ')
+   for edge in predicted_chicago[0] :
+      writer.writerow([edge])
