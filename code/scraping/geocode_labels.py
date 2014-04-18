07e8639c9fa809406f83b3c9ea073298847adce9
Forest Gregg <fgregg@uchicago.edu>
2014-04-18 15:54:56 -0500

    update geocoding for new craigslist format

--- code/scraping/geocode_labels.py
+++ code/scraping/geocode_labels.py
@@ -25,6 +25,39 @@ elif opts.verbose >= 2:
 logging.basicConfig(level=log_level)
 logging.StreamHandler(sys.stdout)
 
+def placeFromMap(listing) :
+    soup = BeautifulSoup(listing)
+
+    geo_location_div = soup.find('div', {'id' : 'map'})
+
+    if geo_location_div :
+        latitude = geo_location_div['data-latitude']
+        longitude = geo_location_div['data-longitude']
+        geolocation = (float(latitude), float(longitude))
+    else :
+        geolocation = None
+
+    address_div = soup.find('div', {'class' : 'mapaddress'})
+
+    if address_div :
+        address = address_div.text
+    else :
+        address = None
+
+    posting_title_div = soup.find('h2', {'class' : 'postingtitle'})
+    
+    if posting_title_div :
+        match = re.search(r'.*\((.+?)\)$', posting_title_div.text.strip())
+        if match :
+            end_label = match.group(1)
+        else :
+            end_label = None
+    else :
+        end_label = None
+
+    return address, geolocation, end_label
+    
+
 def placeFromCLTags(listing) :
     # Craigslists listings have very good meta data tags that
     # look like <-- CLTAG xstreet0==32 W. 21st st -->
@@ -52,11 +85,11 @@ def placeFromCLTags(listing) :
     
     return (cross_street_0, cross_street_1, space)
 
-def addressOrIntersection(cross_street0, cross_street1, city, state) :
+def addressOrIntersection(cross_street0, cross_street1) :
     place = None
     
-    street_address = Template("$street_0, $city $state")
-    intersection = Template("$street_0 & $street_1, $city $state")
+    street_address = Template("$street_0")
+    intersection = Template("$street_0 & $street_1")
 
     if cross_street0 :
         # Sometimes street addresses are written out, particularly
@@ -65,10 +98,7 @@ def addressOrIntersection(cross_street0, cross_street1, city, state) :
         # something that is not a number or whitespace (ideally an
         # letter.
         if re.compile('\d+ [\D\W]').match(cross_street0) :
-            place = street_address.substitute(street_0 = cross_street0,
-                                              city = city,
-                                              state = state)
-            place = place.lower()
+            place = street_address.substitute(street_0 = cross_street0)
         else :
             if cross_street1 and cross_street1 != cross_street0 :
                 # Likely fertile place for improvement
@@ -83,31 +113,24 @@ def addressOrIntersection(cross_street0, cross_street1, city, state) :
                 xstreet0 = xstreet0.title()
                 xstreet1 = xstreet1.title()
                 place = intersection.substitute(street_0 = xstreet0,
-                                                street_1 = xstreet1,
-                                                city = city,
-                                                state = state
-                                                )
-                place = place.lower()
+                                                street_1 = xstreet1)
                 
     return place
 
-def parseListing(listing, city, state) :
+def parseListing(listing) :
     cross_street_0, cross_street_1, geo_area = placeFromCLTags(listing)
     place = addressOrIntersection(cross_street_0, 
-                                  cross_street_1, 
-                                  city, state)
+                                  cross_street_1) 
 
-    return place, geo_area
+    if place is None :
+        place, geolocation, end_label = placeFromMap(listing)
+        if geo_area is None :
+            geo_area = end_label
+    else :
+        geolocation = None
 
-def parseListings(listings, city, state) :
-    place_d = defaultdict(lambda : defaultdict(list))
- 
-    for listing_id, listing in listings :
-        place, geo_area = parseListing(listing, city, state)
-        if place :
-            place_d[place][geo_area].append(listing_id)
+    return place, geolocation, geo_area
 
-    return place_d
 
 def localLookup(cursor, place) :     
         # Geocoding from the Google API is expensive, and a great many of
@@ -151,8 +174,8 @@ def geocode(place) :
 
     return georeference
 
-def deduplicateGeoreference(cursor, georeference) :
-    cursor.execute("""SELECT location_id FROM location where canonical_location = %s""", (georeference[0]))
+def deduplicateGeoreference(cursor, place) :
+    cursor.execute("""SELECT location_id FROM location where canonical_location = %s""", (place))
     location_id = c.fetchone()
     if location_id :
         location_id = location_id[0]
@@ -160,55 +183,20 @@ def deduplicateGeoreference(cursor, georeference) :
     return location_id
 
 
-
-
-
-def updateLocations(cursor, georeference, place) :
+def updateLocations(cursor, place, geolocation) :
     cursor.execute("""REPLACE INTO location """
-                   """(canonical_location, lat_long, city) """
-                   """VALUES (%s, PointFromText('POINT(%s %s)'), %s)""", 
-                   (georeference[0], 
-                    georeference[1][0], 
-                    georeference[1][1], city))
+                   """(canonical_location, lat_long) """
+                   """VALUES (%s, PointFromText('POINT(%s %s)'))""", 
+                   (place, 
+                    geolocation[0], 
+                    geolocation[1]))
     location_id = db.insert_id()
     db.commit()
     
 
     return location_id
 
-
-
-
-db = MySQLdb.connect(db="neighborhood",
-                     read_default_file="~/.my.cnf")
-db.set_character_set('utf8')
-
-c = db.cursor()
-c.execute('SET NAMES utf8;')
-c.execute('SET CHARACTER SET utf8;')
-c.execute('SET character_set_connection=utf8;')
-
-
-db = MySQLdb.connect(db="neighborhood",
-                     read_default_file="~/.my.cnf")
-c = db.cursor()
-
-# Get the listings from rss feeds that are more recent than the
-# rss that contained the last location we inserted
-
-num_cities = len(config.cities)
-
-for k, city in enumerate(config.cities) :
-    c.execute("""SELECT COUNT(*) FROM label""")
-    num_labels_start = c.fetchone()[0]
-
-    logging.info("%(city)s: %(i)d / %(total)d : %(time)s",
-                 {'city' : city, 
-                  'i' : k + 1, 
-                  'total' : num_cities,
-                  'time' : time.strftime("%Y-%m-%d %H:%M:%S")})
-    city_name, state = config.cities[city]
-
+def last_published(c, city) :
     c.execute("""SELECT IFNULL(MIN(published),0) """
               """       FROM listing """
               """       WHERE """
@@ -221,14 +209,9 @@ for k, city in enumerate(config.cities) :
               """                                     WHERE city = %s))""",
               city)
 
-    min_published = c.fetchone()[0]
+    return c.fetchone()[0]
 
-
-    if min_published != 0 :
-        logging.debug(min_published)
-    else :
-        logging.debug("No 'latest' listing can't be found, starting from beginning'")
-
+def new_listings(c, min_published, city) :
     c.execute("""SELECT listing_id, content """
               """FROM listing """
               """WHERE """
@@ -237,51 +220,129 @@ for k, city in enumerate(config.cities) :
               """ORDER BY published""",
               (min_published, city, ))
 
-    listings = c.fetchall()
+    return c.fetchall()
 
-    places = parseListings(listings, city_name, state)
+def lookupLocation(c, place, geolocation) :
+    location_id = None
 
+    if place :
+        location_id = localLookup(c, place)
+
+        if location_id is None :
+            location_id = deduplicateGeoreference(c, place)
+
+        if location_id is None and geolocation is None :
+            georeference = geocode(place)
+            if georeference :
+                normalized_place, geolocation = georeference
+                
+                location_id = lookupLocation(c, 
+                                             normalized_place, 
+                                             geolocation)
+
+        if location_id is None and geolocation :
+            location_id = updateLocations(c,
+                                          place,
+                                          geolocation)
+
+    elif geolocation :
+        location_id = updateLocations(c,
+                                      '',
+                                      geolocation)
+
+    
+    return location_id 
+
+            
+    
+
+
+def label_count(c) :
+    c.execute("""SELECT COUNT(*) FROM label""")
+    return c.fetchone()[0]
+
+
+if __name__ == '__main__' :
     geo = geocoders.GeocodeFarm(SECRET.API_KEY)
 
-    for i, place in enumerate(places) :
-        logging.debug(" Place: %(i)i: %(place)s", {'i':i, 'place':place})
+    db = MySQLdb.connect(db="neighborhood",
+                         read_default_file="~/.my.cnf")
+    db.set_character_set('utf8')
 
-        location_id = localLookup(c, place)
-        if location_id :
-            logging.debug("  place found place in db")
+    c = db.cursor()
+    c.execute('SET NAMES utf8;')
+    c.execute('SET CHARACTER SET utf8;')
+    c.execute('SET character_set_connection=utf8;')
+
+
+    db = MySQLdb.connect(db="neighborhood",
+                         read_default_file="~/.my.cnf")
+    c = db.cursor()
+
+    # Get the listings from rss feeds that are more recent than the
+    # rss that contained the last location we inserted
+
+    num_cities = len(config.cities)
+
+    for k, city in enumerate(config.cities) :
+        logging.info("%(city)s: %(i)d / %(total)d : %(time)s",
+                     {'city' : city, 
+                      'i' : k + 1, 
+                      'total' : num_cities,
+                      'time' : time.strftime("%Y-%m-%d %H:%M:%S")})
+
+        city_name, state = config.cities[city]
+
+        num_labels_start = label_count(c)
+
+        last_listing = last_published(c, city)
+
+        if last_listing != 0 :
+            logging.debug(last_listing)
         else :
-            georeference = geocode(place)
-            if georeference is None :
+            logging.debug("No 'latest' listing can't be found, starting from beginning'")
+
+        listings = new_listings(c, last_listing, city)
+
+        i = 0
+        for listing_id, listing in listings :
+            try :
+                place, geolocation, label = parseListing(listing)
+            except :
+                print listing
+                print listing_id
+                raise
+
+            if not place and not geolocation :
                 continue
 
-            location_id = deduplicateGeoreference(c, georeference)
-
-            if location_id :
-                logging.debug("  location found in db")
-                updateLookup(c, location_id, place)
+            i += 1
+            
+            if place :
+                logging.debug(" Place: %(i)i: %(place)s", {'i':i, 'place':place})
+                place = "%s, %s %s" % (place.lower().strip(), city, state)
 
             else :
-                location_id = updateLocations(c, georeference, place)
-                updateLookup(c, location_id, place)
+                logging.debug(" Place: %(i)i: No address", {'i':i})
 
+            location_id = lookupLocation(c, place, geolocation)
+            
+            if location_id and label :
+                logging.debug("  label: %(label)s", {"label" :label})
 
-        # Now update the label table
-        for label in places[place] :
-            logging.debug("  label: %(label)s", {"label" :label})
-            for listing_id in places[place][label] :
                 c.execute(""" REPLACE INTO label """
                           """(location_id, listing_id, label) """
                           """VALUES (%s, %s, %s) """, 
-                          (location_id, listing_id, label))
+                                  (location_id, listing_id, label))
                 db.commit()
 
-    c.execute("""SELECT COUNT(*) FROM label""")
-    num_labels_end = c.fetchone()[0]
-    
-    logging.info("%(new labels)d new labels", 
-                 {'new labels' : num_labels_end - num_labels_start})
+        c.execute("""SELECT COUNT(*) FROM label""")
+        num_labels_end = c.fetchone()[0]
 
-            
-c.close()
-db.close()    
+        logging.info("%(new labels)d new labels", 
+                     {'new labels' : num_labels_end - num_labels_start})
+
+
+    c.close()
+    db.close()    
 
